8ff0cd9c4acf480a0621eacff7c4e671414a0ceb
TranHuuHien <thhien@ctu.edu.vn>
2020-05-14 13:17:52 +0700

    Tạo expect và actual class

--- shared/src/commonMain/kotlin/example/kotlinreport/multiplatform/shared/Factory.kt
+++ shared/src/commonMain/kotlin/example/kotlinreport/multiplatform/shared/Factory.kt
@@ -4,6 +4,12 @@ expect class Product {
     val user: String
 }
 
+expect class Userx {
+    var id: String
+    var name: String
+    var sex: String
+}
+
 expect object Factory {
     fun create(config: Map<String, String>): Product
     val platform: String
