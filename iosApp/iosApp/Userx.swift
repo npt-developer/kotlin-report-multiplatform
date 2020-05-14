463d42e57eb310c1fdb83592ac402a19f672f3ad
Nhan Kim Long <nklong@ctu.edu.vn>
2020-05-14 15:09:06 +0700

    Thêm Userx

---
+++ iosApp/iosApp/Userx.swift
@@ -0,0 +1,15 @@
+//
+//  Userx.swift
+//  iosApp
+//
+//  Created by Cusc Software on 5/14/20.
+//
+
+import Foundation
+
+class Userx
+{
+    var name: String = ""
+    var sex: String = "1"
+    var avatar: String = ""
+}
