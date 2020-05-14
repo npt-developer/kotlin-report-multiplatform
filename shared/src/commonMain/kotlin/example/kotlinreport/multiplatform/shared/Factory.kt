f6db5a8d74391c3392a025e71bdd75a6b23814af
TranHuuHien <thhien@ctu.edu.vn>
2020-05-14 13:29:32 +0700

    Revert change

--- shared/src/commonMain/kotlin/example/kotlinreport/multiplatform/shared/Factory.kt
+++ shared/src/commonMain/kotlin/example/kotlinreport/multiplatform/shared/Factory.kt
@@ -4,12 +4,6 @@ expect class Product {
     val user: String
 }
 
-expect class Userx {
-    var id: String
-    var name: String
-    var sex: String
-}
-
 expect object Factory {
     fun create(config: Map<String, String>): Product
     val platform: String
